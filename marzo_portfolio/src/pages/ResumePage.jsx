f6fe436bc7a3e654f2469b78275fdca36f116991
gmarzo <garrett.marzo@gmail.com>
2024-01-17 15:44:53 -0800

    Working with resume, attempting to switch to pdf format

--- marzo_portfolio/src/pages/ResumePage.jsx
+++ marzo_portfolio/src/pages/ResumePage.jsx
@@ -1,8 +1,35 @@
 import * as React from "react";
 import "../App.css";
+import resume from "../images/resume.jpg";
+import MarzoResume from "../images/MarzoResume.pdf";
+
+import { Document, Page } from "react-pdf";
+
+import { Paper } from "@mui/material";
 
 const ResumePage = () => {
-  return <img src="../images/resume.jpg" alt="It should be here..." />;
+  const [loading, setLoading] = React.useState(true);
+
+  return (
+    <div className="Resume-container">
+      <Paper className="Resume-img" elevation={3}>
+        {/* {loading ? (
+          <Document
+            file={MarzoResume}
+            onLoadSuccess={() => {
+              setLoading(false);
+              console.log("Done");
+            }}
+          >
+            <Page />
+          </Document>
+        ) : (
+          <p>Document Loading...</p>
+        )} */}
+        <img src={resume} alt="Loading Document..." className="Resume-img" />
+      </Paper>
+    </div>
+  );
 };
 
 export default ResumePage;
