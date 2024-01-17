261bf8689c0d09504d9523603bbecd93d95796da
gmarzo <garrett.marzo@gmail.com>
2024-01-17 11:15:23 -0800

    Changed page structure and adding images

--- marzo_portfolio/src/pages/Homepage.jsx
+++ marzo_portfolio/src/pages/Homepage.jsx
@@ -1,43 +1,142 @@
 import * as React from "react";
 import "../App.css";
 
-import {Link} from "react-router-dom"
-import {Paper} from "@mui/material"
+import { Paper } from "@mui/material";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+import AboutPage from "./AboutPage";
+import CodingPage from "./CodingPage";
+import GamesPage from "./GamesPage";
+import ResumePage from "./ResumePage";
+
+const PAGE_ACTIONS = {
+  SET_PAGE: "set-page",
+};
+
+const initialState = {
+  page: "about",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case PAGE_ACTIONS.SET_PAGE:
+      return { ...state, ...action.payload };
+    default:
+      alert("Something went wrong");
+  }
+};
+
+const contentMapping = {
+  about: AboutPage,
+  games: GamesPage,
+  coding: CodingPage,
+  resume: ResumePage,
+};
 
 const Homepage = () => {
-  const [clicked, setClicked] = React.useState(false)
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
+  const Content = contentMapping[state.page];
   return (
     <div className="Landing-page">
-      <div className="Banner">
-        <h1 className="Banner-title">Hello World</h1>
-        <h2 className="Banner-subtitle">My Name is Garrett</h2>
-      </div>
+      <HelmetProvider>
+        <Helmet>
+          <title>Garrett Marzo</title>
+        </Helmet>
+        <div className="Banner">
+          <h1 className="Banner-title">Hello World</h1>
+          <h2 className="Banner-subtitle">My Name is Garrett</h2>
+        </div>
 
-      <div className="Showcase">
-        <div className="Showcase-container">
-          <Link to={"games"}>
-            <Paper className="Portal" elevation={3}>
-              <p className="Portal-title">Game Projects</p>
-              <p>WIP</p>
-            </Paper>
-          </Link>
+        {/* Main Page switchable content */}
+        <div className="Showcase">
+          {/* <div className="Showcase-container">
+            <Link to={"games"}>
+              <Paper className="Portal" elevation={3}>
+                <p className="Portal-title">Game Projects</p>
+                <p>WIP</p>
+              </Paper>
+            </Link>
 
-          <Link to={"code"}>
-            <Paper className="Portal" elevation={3}>
-              <p className="Portal-title">Coding Projects</p>
-              <p>WIP</p>
-            </Paper>
-          </Link>
-          
-          <Link to={"about"}>
-            <Paper className="Portal" elevation={3}>
-              <p className="Portal-title">About</p>
-              <p>WIP</p>
-            </Paper>
-          </Link>
-        </div>  
-      </div>
+            <Link to={"code"}>
+              <Paper className="Portal" elevation={3}>
+                <p className="Portal-title">Coding Projects</p>
+                <p>WIP</p>
+              </Paper>
+            </Link>
+
+            <Link to={"about"}>
+              <Paper className="Portal" elevation={3}>
+                <p className="Portal-title">About</p>
+                <p>WIP</p>
+              </Paper>
+            </Link>
+          </div> */}
+          <div className="Page-container">
+            {Content ? (
+              <Content
+                mapDispatch={dispatch}
+                mapState={state}
+                PAGE_ACTIONS={PAGE_ACTIONS}
+              />
+            ) : (
+              <div>Sorry, something went wrong</div>
+            )}
+          </div>
+          {/* Side Navigation Bar */}
+          <div className="Nav-container">
+            <h3
+              className="Page-setter"
+              onClick={() =>
+                dispatch({
+                  type: PAGE_ACTIONS.SET_PAGE,
+                  payload: { page: "games" },
+                })
+              }
+            >
+              {" "}
+              Game Projects
+            </h3>
+
+            <h3
+              className="Page-setter"
+              onClick={() =>
+                dispatch({
+                  type: PAGE_ACTIONS.SET_PAGE,
+                  payload: { page: "coding" },
+                })
+              }
+            >
+              {" "}
+              Coding Projects
+            </h3>
+
+            <h3
+              className="Page-setter"
+              onClick={() =>
+                dispatch({
+                  type: PAGE_ACTIONS.SET_PAGE,
+                  payload: { page: "about" },
+                })
+              }
+            >
+              About Me
+            </h3>
+
+            <h3
+              className="Page-setter"
+              onClick={() =>
+                dispatch({
+                  type: PAGE_ACTIONS.SET_PAGE,
+                  payload: { page: "resume" },
+                })
+              }
+            >
+              Resume
+            </h3>
+          </div>
+        </div>
+      </HelmetProvider>
     </div>
   );
 };
---
+++ marzo_portfolio/src/pages/ResumePage.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import "../App.css";
+
+const ResumePage = () => {
+  return <img src="../images/resume.jpg" alt="It should be here..." />;
+};
+
+export default ResumePage;
